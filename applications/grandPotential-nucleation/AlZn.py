504ee0f3cfe43de3c60264cc933c20c45db1ab6f
Xander <102053371+fractalsbyx@users.noreply.github.com>
2024-08-21 01:26:29 -0400

    Update AlZn.py

--- applications/grandPotential-nucleation/AlZn.py
+++ applications/grandPotential-nucleation/AlZn.py
@@ -15,8 +15,8 @@ AlZn_Sys.fromTDB(db, elements, component, temperature)
 AlZn_Fit = BS.BinaryIsothermal2ndOrderSystem()
 AlZn_Fit.from_discrete_near_equilibrium(AlZn_Sys, x=0.35)
 
-print(AlZn_Sys.phases)
-print(AlZn_Fit.phases)
+print(AlZn_Sys.phases.keys())
+print(AlZn_Fit.phases.keys())
 skew = AlZn_Sys.phases['FCC_A1'].xdata*0#8000
 plt.plot(AlZn_Sys.phases['FCC_A1'].xdata,
          AlZn_Fit.phases['FCC_A1_0'].free_energy(AlZn_Sys.phases['FCC_A1'].xdata)+skew,
@@ -33,13 +33,10 @@ plt.plot(AlZn_Sys.phases['FCC_A1'].xdata,
          linestyle='-', color='k',
          label="FCC_A1",linewidth=1)
 
-#plt.ylim([-56000,-47000])
 plt.xlim([0,1])
 #plt.xticks([])
-plt.show()
 #plt.yticks([])
 
 plt.savefig(fname="fit.png")
 
 BS.write_binary_isothermal_parabolic_parameters(AlZn_Fit, output_file="AlZn.prm", template_file="template.prm", component="ZN")
-
